bc03e031f529c4fd31c69af82012959b65e29816
suvajitroyofficial <suvajitroy66083@gmail.com>
2023-01-28 21:09:13 +0530

    frist commit

---
+++ suvajit2.java
@@ -0,0 +1,16 @@
+public class HelloWorld{
+
+	public static void main(String[] args) {
+		int num1;
+		num = 7;
+		int num2 = 21;
+		int sum = num1+num2;
+		
+		int sum = num1=num2;
+
+		System.out.println("The Sum is: "+ sum);
+
+
+	}	
+
+}
