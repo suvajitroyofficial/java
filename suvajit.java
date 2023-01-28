3acea5a94716a03857981d430e8ba45f12797064
suvajitroyofficial <suvajitroy66083@gmail.com>
2023-01-28 21:25:00 +0530

    Suuvajit Modifide

--- suvajit.java
+++ suvajit.java
@@ -13,4 +13,17 @@ public class HelloWorld{
 
 	}	
 
+	public static void main(String[] args) {
+		int num1;
+		num = 7;
+		int num2 = 21;
+		int sum = num1+num2;
+		
+		int sum = num1=num2;
+
+		System.out.println("The Sum is: "+ sum);
+
+
+	}
+
 }
